edc383f084edf16318e127790a6c9b0f35c7f293
Jamie Witt <7908029+wittjr@users.noreply.github.com>
2024-11-22 16:58:24 -0500

    setup for mysql for non-dev and update footer

--- gameworn/settings.py
+++ gameworn/settings.py
@@ -1,1 +1,13 @@
 from .base_settings import *
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": os.environ.get('DB_CONNECTION_INFO'),
+        },
+    }
+}
