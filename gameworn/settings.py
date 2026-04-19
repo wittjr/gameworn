11cc1fb29a9be54df68c647b1e58783d07bd6337
Jamie Witt <7908029+wittjr@users.noreply.github.com>
2026-04-19 16:47:52 -0400

    add some logging

--- gameworn/settings.py
+++ gameworn/settings.py
@@ -15,3 +15,22 @@ DATABASES = {
         },
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "/home/yourusername/django-error.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
