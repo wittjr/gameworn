827204c7a644eced177edf36a7cf2bf37863e8cd
Jamie Witt <7908029+wittjr@users.noreply.github.com>
2026-04-19 19:58:18 -0400

    remove logging changes

--- gameworn/settings.py
+++ gameworn/settings.py
@@ -15,22 +15,3 @@ DATABASES = {
         },
     }
 }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'error_file': {
-            'level': 'ERROR',  # Set to ERROR to capture only error messages
-            'class': 'logging.FileHandler',
-            'filename': os.environ['LOG_FILE'],  # Specify your error log file path
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['error_file'],
-            'level': 'ERROR',  # Set to ERROR to ensure only errors are logged
-            'propagate': True,
-        },
-    },
-}
